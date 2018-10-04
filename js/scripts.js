93ae6270925aef9ed335c0e8f9342ecfbcbf9c9d
Jentry J and Michael Borthwick <michael@>
2018-10-03 20:26:55 -0700

    add calebs code thx caleb https://github.com/CalebPaul/pig-latin

--- js/scripts.js
+++ js/scripts.js
@@ -9,60 +9,36 @@
 //   }
 // };
 
-$(document).ready(function() {
-  $("#blanks form").submit(function(event) {
-    event.preventDefault();
-      var sentenceInput = $("input#sentence").val();
-      console.log(sentenceInput);
 
-      var sentenceArray = sentenceInput.split(" ");
-      console.log(sentenceArray);
+//this is our code
+// $(document).ready(function() {
+//   $("#blanks form").submit(function(event) {
+//     event.preventDefault();
+//       var sentenceInput = $("input#sentence").val();
+//       // console.log(sentenceInput);
+//
+//       var sentenceArray = sentenceInput.split(" ");
+//       // console.log(sentenceArray);
+//
+//       var textOutput = sentenceArray.forEach(function(word){
+//         if (word[0] === "a" || word[0] === "e" || word[0] === "i" || word[0] === "o" || word[0] === "u"){
+//           return word + "way";
+//           console.log(word + "way");
+//         } else {
+//           return console.log("nope");
+//         };
+//         // return console.log(word + "way");
+//
+//       });
+//
+// var result = $(".result").append(textOutput());
+//
+//   });
+// });
+//end of our code
 
-      var textOutput = sentenceArray.forEach(function(word){
-        if (word[0] === "a" || word[0] === "e" || word[0] === "i" || word[0] === "o" || word[0] === "u"){
-          // return word + "way";
-          console.log(word + "way");
-        } else {
-          return console.log("nope");
-        };
-        // return console.log(word + "way");
 
 
-      });
-
-
-      // var result = textOutput();
-      // console.log(result);
-
-      // function(){
-      //
-      //
-      //
-      //
-      //
-      //   for (var i = 0; i < sentenceArray.length; i++){
-      //     if (sentenceArray[0].includes("a") || sentenceArray[0].includes("e")){
-      //       return sentenceArray + "way";
-      //     } else {
-      //       return "hello";
-      //     }
-      //
-      //
-      //
-      //
-      //   };
-      //
-      //   if (sentenceArray[0] === "a" || sentenceArray[0] === "e" || sentenceArray[0] === "i" || sentenceArray[0] === "o" || sentenceArray[0] === "u") {
-      //     return sentenceInput + "way";
-      //   } else {
-      //       return "hello";
-      //   };
-      //
-      // };
-
-  });
-
-});
 
 
 //UI Logic
@@ -70,6 +46,7 @@ $(document).ready(function() {
 // console.log(textInput);
 //
 // var result = textOutput();
+// // $(".result").attr(result);
 // console.log(result);
 
 
@@ -80,3 +57,42 @@ $(document).ready(function() {
 //   default:
 //    return sentenceInput + "?";
 //  }
+
+
+
+
+var firstFunction = function(phrase) {
+  var phraseArray = phrase.split(" ");
+  console.log(phraseArray);
+  var newArray=phraseArray.map(translate);
+
+  //pause + step into the other function
+  phrase = newArray.join(" ");
+  console.log(phrase);
+  return phrase;
+}
+
+function translate(word) {
+  for(var i=0; i<word.length; i++) {
+    if (!"AEIOUaeiou".includes(word[0])) {
+      if (((word.substr(0,1)==="q")||("Q"===word.substr(0,1)))&&
+          ((word.substr(1,1)==="u")||("U"===word.substr(1,1)))) {
+          word = word.substr(2) + word.substr(0,2);
+      } else {
+        word = word.substr(1) + word.substr(0,1);
+      }
+    } else {
+      word+="ay";
+      return word;
+    }
+  }
+}
+
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+    var userInput = $("#phrase").val();
+
+    var result = $("#results").text(firstFunction(userInput));
+  });
+});
