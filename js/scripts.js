68fea9b882900398a31793a2ad147c6e0b77ddd4
Jentry J and Michael Borthwick <michael@>
2018-10-03 19:25:33 -0700

    create function that adds way to each word in an array

--- js/scripts.js
+++ js/scripts.js
@@ -14,19 +14,52 @@ $(document).ready(function() {
     event.preventDefault();
       var sentenceInput = $("input#sentence").val();
       console.log(sentenceInput);
-      var sentenceArray = sentenceInput.split("");
+
+      var sentenceArray = sentenceInput.split(" ");
       console.log(sentenceArray);
 
-      var textOutput = function(){
-        if (sentenceArray[0] === "a" || sentenceArray[0] === "e") {
-          return sentenceInput + "way";
+      var textOutput = sentenceArray.forEach(function(word){
+        if (word[0] === "a" || word[0] === "e" || word[0] === "i" || word[0] === "o" || word[0] === "u"){
+          // return word + "way";
+          console.log(word + "way");
         } else {
-            return "hello";
+          return console.log("nope");
         };
+        // return console.log(word + "way");
 
-      };
-        var result = textOutput(sentenceArray);
-        console.log(result);
+
+      });
+
+
+      // var result = textOutput();
+      // console.log(result);
+
+      // function(){
+      //
+      //
+      //
+      //
+      //
+      //   for (var i = 0; i < sentenceArray.length; i++){
+      //     if (sentenceArray[0].includes("a") || sentenceArray[0].includes("e")){
+      //       return sentenceArray + "way";
+      //     } else {
+      //       return "hello";
+      //     }
+      //
+      //
+      //
+      //
+      //   };
+      //
+      //   if (sentenceArray[0] === "a" || sentenceArray[0] === "e" || sentenceArray[0] === "i" || sentenceArray[0] === "o" || sentenceArray[0] === "u") {
+      //     return sentenceInput + "way";
+      //   } else {
+      //       return "hello";
+      //   };
+      //
+      // };
+
   });
 
 });
